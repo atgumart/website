6747c64e77a30b3e45d03056a158206473def724
atgumart <gustavo@gumart.com.br>
2022-05-25 14:33:20 -0300

    update convert for thumbnails

--- convert.sh
+++ convert.sh
@@ -2,6 +2,10 @@
 
 find . -type f -name '*.jpg' | while read -r line; do
     printf 'converting:\t%s' "$line"
-    convert "$line" \( -clone 0 -resize 1280x720\! -blur 0x9 \) \( -clone 0 -resize 1280x720 \) -delete 0 -gravity center -compose over -composite -quality 75 "$line"
+    if [ "${line##.*/}" = 'thumbnail.jpg' ]; then
+        convert "$line" \( -clone 0 -resize 640x360\! -blur 0x9 \) \( -clone 0 -resize 640x360 \) -delete 0 -gravity center -compose over -composite -quality 50 "$line"
+    else
+        convert "$line" \( -clone 0 -resize 1280x720\! -blur 0x9 \) \( -clone 0 -resize 1280x720 \) -delete 0 -gravity center -compose over -composite -quality 75 "$line"
+    fi
     echo
 done

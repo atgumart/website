0a94ae944a0f94a38fd6005a00bdb96f57f8dbc9
atgumart <gustavo@gumart.com.br>
2022-05-26 10:29:11 -0300

    update new form and db changes

---
+++ assets/js/add.js
@@ -0,0 +1,72 @@
+let db = []
+
+const priceMask = IMask(priceInput, {
+    mask:"R$ num",
+    blocks: {
+        num: {
+            mask: Number,
+            thousandsSeparator: "."
+        }
+    }
+})
+
+const areaMask = IMask(areaInput, {
+    mask: Number,
+    thousandsSeparator: "."
+})
+
+populateLocationDatalist()
+
+sellButton.addEventListener("mouseover", (envent => {
+    sellButton.classList.replace("btn-outline-primary", "btn-primary")
+    rentButton.classList.replace("btn-primary", "btn-outline-primary")
+}), false)
+
+sellButton.addEventListener("mouseout", (envent => {
+    sellButton.classList.replace("btn-primary", "btn-outline-primary")
+    rentButton.classList.replace("btn-outline-primary", "btn-primary")
+}), false)
+
+addForm.addEventListener("submit", (event => {
+    event.preventDefault()
+    event.stopPropagation()
+    if (addForm.checkValidity()) {
+        genZip(event.submitter.value)
+    }
+    addForm.classList.add("was-validated")
+}), false)
+
+async function populateLocationDatalist() {
+    db = await fetch("/id/db.json").then(r => r.json())
+    const options = new Set(db.map(db => db.location).sort((a, b) => {
+        return a.localeCompare(b)
+    }))
+    options.forEach(option => {
+        locationDatalist.innerHTML += `<option value="${option}"></option>`
+    })
+}
+
+function genZip(action) {
+    const basedir = "id"
+    const zip = new JSZip()
+    const id = Date.now().toString(16)
+    zip.file(`${basedir}/${id}.json`, JSON.stringify({
+        id: id,
+        location: locationInput.value,
+        action: action,
+        price: parseInt(priceMask.unmaskedValue),
+        bedrooms: parseInt(bedroomsInput.value),
+        bathrooms: parseInt(bathroomsInput.value),
+        spots: parseInt(spotsInput.value),
+        area: parseInt(areaMask.unmaskedValue),
+        description: descriptionText.value,
+        pics: picsInput.files.length
+    }))
+    zip.file(`${basedir}/${id}/thumbnail.jpg`, thumbnailInput.files[0], { base64: true })
+    for (let i = 0; i < picsInput.files.length; i++) {
+        zip.file(`${basedir}/${id}/${i + 1}.jpg`, picsInput.files[i], { base64: true })
+    }
+    zip.generateAsync({ type: "blob" }).then(content => {
+        saveAs(content, `${id}.zip`)
+    })
+}
--- assets/js/gumart.js
+++ assets/js/gumart.js
@@ -5,32 +5,38 @@ loadIndex()
 
 async function loadIndex() {
     db = await fetch(`${basedir}/db.json`).then(r => r.json())
+    const options = new Set(db.map(db => db.location).sort((a, b) => {
+        return a.localeCompare(b)
+    }))
+    options.forEach(option => {
+        locationSelect.innerHTML += `<option value="${option}">${option}</option>`
+    })
     loadItems("sell")
 }
 
-function loadItems(type) {
+function loadItems(action) {
     itemsRow.innerHTML = ""
     new bootstrap.Collapse(filterCollapse).toggle()
-    if (type == "rent") {
+    if (action == "rent") {
         rentButton.classList.replace("btn-outline-primary", "btn-primary")
         buyButton.classList.replace("btn-primary", "btn-outline-primary")
-    } else if (type == "sell") {
+    } else if (action == "sell") {
         buyButton.classList.replace("btn-outline-primary", "btn-primary")
         rentButton.classList.replace("btn-primary", "btn-outline-primary")
     }
     db.filter(item => {
-        return item.bedrooms >= bedroomsSelect.value &&
+        return item.location.match(locationSelect.value) &&
+        item.bedrooms >= bedroomsSelect.value &&
         item.bathrooms >= bathroomsSelect.value &&
         item.spots >= spotsSelect.value &&
-        item.area >= areaInput.value &&
-        item.type == type
+        item.action == action
     }).sort((a, b) => b.price - a.price).forEach(item => {
         itemsRow.innerHTML += `
             <div class="col">
                 <div class="card shadow-sm">
                     <a class="stretched-link" href="#modal" data-bs-toggle="modal" onclick="updateModal('${item.id}')"><img src="${basedir}/${item.id}/thumbnail.jpg" alt="thumbnail" class="card-img-top"></a>
                     <div class="card-body text-center py-2">
-                        <h5 class="card-title my-1">${item.district}</h5>
+                        <h5 class="card-title my-1">${item.location}</h5>
                         <ul class="list-group list-group-horizontal justify-content-center card-subtitle fw-bold my-0">
                             <li class="list-group-item border-0">${item.bedrooms} <i class="fa-solid fa-bed"></i></li>
                             <li class="list-group-item border-0">${item.bathrooms} <i class="fa-solid fa-toilet"></i></li>
@@ -59,8 +65,8 @@ function updateModal(id) {
         `
     }
 
-    modalDistrict.innerHTML = `<i class="fa-solid fa-location-dot text-danger"></i> ${item.district}`
-    modalPrice.innerHTML = `${(item.type == "rent") ? "Alugar" : "Comprar"} por <span class="fw-bold">${item.price.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}</span>`
+    modalLocation.innerHTML = `<i class="fa-solid fa-location-dot text-danger"></i> ${item.location}`
+    modalPrice.innerHTML = `${(item.action == "rent") ? "Alugar" : "Comprar"} por <span class="fw-bold">${item.price.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}</span>`
     modalAttributes.innerHTML = `
         <li class="list-group-item border-0">${item.bedrooms} <i class="fa-solid fa-bed"></i></li>
         <li class="list-group-item border-0">${item.bathrooms} <i class="fa-solid fa-toilet"></i></li>
